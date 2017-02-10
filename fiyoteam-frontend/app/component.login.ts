d43173559b903d08ffc7c4b925b0c54b9c8319cb
Mate Zoltan <zoltanaty@gmail.com>
2017-02-10 22:00:29 +0200

    [task] Error handling on login.

--- fiyoteam-frontend/app/component.login.ts
+++ fiyoteam-frontend/app/component.login.ts
@@ -5,38 +5,46 @@ import {GetAndPostService, User} from './service.getandpost'
 
 
 @Component({
-    selector: 'login',
-    templateUrl: 'app/templates/login.template.html',
-    providers: [GetAndPostService]
+	selector: 'login',
+	templateUrl: 'app/templates/login.template.html',
+	providers: [GetAndPostService]
 
 })
 export class LoginComponent { 
 
 	public user = new User(null, '','');
-    private ipv4 = "localhost";
+	private ipv4 = "localhost";
+
+	private loginError: false;
 
 	constructor(private _router: Router, private getAndPostService:GetAndPostService) {}
 
 	login() {
-        this.getAndPostService.postData(this.user, 'http://' + this.ipv4 + ':8080/fiyoteam-backend/rest/authentication/login').map(res => res.json())
+		this.getAndPostService.postData(this.user, 'http://' + this.ipv4 + ':8080/fiyoteam-backend/rest/authentication/login').map(res => res.json())
 
-        .subscribe(
-          (res) => {
-            if(res.id >= 0 && res.role=="user"){
-                this._router.navigate(['MainUser']); 
-                localStorage.setItem("USERID", res.id);
-                localStorage.setItem("USEREMAIL", res.email);
+		.subscribe(
+			(res) => {
+				if(res.id >= 0 && res.role=="user"){
+					this._router.navigate(['MainUser']); 
+					localStorage.setItem("USERID", res.id);
+					localStorage.setItem("USEREMAIL", res.email);
+					this.loginError = false;
 
-            }else if(res.id >= 0 && res.role=="admin"){
-                this._router.navigate(['MainAdmin']);    
-                localStorage.setItem("USERID", res.id);
-                localStorage.setItem("USEREMAIL", res.email);
+				}else if(res.id >= 0 && res.role=="admin"){
+					this._router.navigate(['MainAdmin']);    
+					localStorage.setItem("USERID", res.id);
+					localStorage.setItem("USEREMAIL", res.email);
+					this.loginError = false;
 
-            }else if(res.id < 0){
-                console.log("Failed to Login :(");
-            }
-            
-        }
-        );
-    }
+				}else if(res.id < 0){
+					this.loginError = true;
+				}
+			}
+			);
+	}
+
+
+	disableError(value){
+		this.loginError = false;
+	}
 }
