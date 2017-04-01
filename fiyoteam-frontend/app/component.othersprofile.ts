5681c1e0420c47285a77af9500dad70a4161456d
Mate Zoltan <zoltanaty@gmail.com>
2017-04-01 13:53:25 +0300

    [task] Others profile view finished.

--- fiyoteam-frontend/app/component.othersprofile.ts
+++ fiyoteam-frontend/app/component.othersprofile.ts
@@ -2,14 +2,14 @@ import {Component, ViewChild } from 'angular2/core';
 import {Router} from 'angular2/router';
 import {Observable} from 'rxjs/Rx';
 import {GetAndPostService, User} from './service.getandpost'
-import {LanguageTemplateComponent} from './component.languages';
-import {SkillTemplateComponent} from './component.skills';
+import {OthersLanguageTemplateComponent} from './component.otherslanguages';
+import {OthersSkillTemplateComponent} from './component.othersskills';
 import {RatingComponent} from './component.rating'
 
 @Component({
 	selector: 'othersprofile',
 	templateUrl: 'app/templates/othersprofile.template.html',
-	directives: [LanguageTemplateComponent, SkillTemplateComponent, RatingComponent],
+	directives: [OthersLanguageTemplateComponent, OthersSkillTemplateComponent, RatingComponent],
 	providers: [GetAndPostService]
 
 })
@@ -18,7 +18,6 @@ export class OthersProfileComponent {
 
 	private userId;
 	private user = new User(null, '', '', '','', '', '', '', '', '');
-	private filesToUpload: Array<File> = [];
 	private profilePicURL: string = null;
 
 	constructor(private _router: Router, private getAndPostService: GetAndPostService) {}
@@ -37,59 +36,6 @@ export class OthersProfileComponent {
 				this.user = res;
 			}
 			);
-	}
-
-	updateUser() {
-
-		this.getAndPostService.postData(this.user, this.getAndPostService.baseUrl + 'user/' + this.userId).map(res => res.json())
-
-		.subscribe(
-			(res) => {
-				this.user = res;
-				this.showUpdatedMessage();
-			}
-			);
-	}
-
-	showUpdatedMessage(){
-		this.showUpdatedUserMessage = true;
-	}
-
-	hideUpdatedMessage(){
-		this.showUpdatedUserMessage = false;
-	}
-
-	upload() {
-		this.makeFileRequest( this.getAndPostService.baseUrl + 'user/profilepic/' + this.userId, this.filesToUpload).then((result) => {
-			this.createImageURL();
-		}, (error) => {
-			console.error(error);
-		});
-	}
-
-	fileChangeEvent(fileInput: any){
-		this.filesToUpload = <Array<File>> fileInput.target.files;
-	}
-
-	makeFileRequest(url: string, files: Array<File>) {
-		return new Promise((resolve, reject) => {
-			var formData: any = new FormData();
-			var xhr = new XMLHttpRequest();
-			for(var i = 0; i < files.length; i++) {
-				formData.append("file", files[i], files[i].name);
-			}
-			xhr.onreadystatechange = function () {
-				if (xhr.readyState == 4) {
-					if (xhr.status == 200) {
-						resolve(JSON.parse(xhr.response));
-					} else {
-						reject(xhr.response);
-					}
-				}
-			}
-			xhr.open("POST", url, true);
-			xhr.send(formData);
-		});
 	}
 
 	downloadImage(url:string){ 
