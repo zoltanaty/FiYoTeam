c4b509921ca88899683b03c16868c5f6ba7178cb
Mate Zoltan <zoltanaty@gmail.com>
2017-04-02 16:18:54 +0300

    [task] CorsFilter modified.

--- src/main/java/com/fiyoteam/utils/CorsFilter.java
+++ src/main/java/com/fiyoteam/utils/CorsFilter.java
@@ -9,7 +9,7 @@ public class CorsFilter implements ContainerResponseFilter {
 	@Override
 	public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
 
-		response.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
+		response.getHttpHeaders().add("Access-Control-Allow-Origin", "http://localhost:3000");
 		response.getHttpHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
 		response.getHttpHeaders().add("Access-Control-Allow-Credentials", "true");
 		response.getHttpHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
