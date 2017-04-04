2e68315e3c431261fac293e672fd457cae32bf51
Mate Zoltan <zoltanaty@gmail.com>
2017-04-04 20:21:58 +0300

    [task] app.js modified.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
-var server = require('http').Server(app);
 
-server.listen(process.env.PORT || 9999,function(){
-    console.log("Server connected. Listening on port: 9999");
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/fiyoteam'));
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
-
-app.use( express.static(__dirname + '/fiyoteam' ) );
-
-
