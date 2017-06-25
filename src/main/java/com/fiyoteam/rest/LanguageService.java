bfd23cc5cf8cc1276c148617065384fc349ac049
Mate Zoltan <zoltanaty@gmail.com>
2017-06-25 12:24:23 +0300

    [task] Admin functionalities implemented

--- src/main/java/com/fiyoteam/rest/LanguageService.java
+++ src/main/java/com/fiyoteam/rest/LanguageService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -13,7 +15,7 @@ import javax.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fiyoteam.model.DTO.UserLanguageDTO.Language;
+import com.fiyoteam.model.Language;
 import com.fiyoteam.persistence.Entitymanager;
 import com.sun.jersey.spi.container.ResourceFilters;
 
@@ -37,6 +39,50 @@ public class LanguageService {
 
 		Entitymanager.closeEntityManager();
 		return Response.ok(languageList).build();
+
+	}
+	
+	@GET
+	@Path("/{newLanguage}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ResourceFilters(MyRequestFilter.class)
+	public Response addNewLanguage(@PathParam("newLanguage") String newLanguage) {
+		EntityManager em = Entitymanager.getEntityManagerInstance();
+		
+		Language language = new Language();
+		language.setId(0);
+		language.setLanguage(newLanguage);
+		
+		em.getTransaction().begin();
+		em.persist(language);
+		em.flush();
+		em.getTransaction().commit();
+
+		log.info("New language added by Admin");
+
+		Entitymanager.closeEntityManager();
+		return getAllLanguages();
+
+	}
+	
+	@DELETE
+	@Path("/{languageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ResourceFilters(MyRequestFilter.class)
+	public Response deleteLanguage(@PathParam("languageId") Integer languageId) {
+		EntityManager em = Entitymanager.getEntityManagerInstance();
+		
+		Language toDelete = em.find(Language.class, languageId);
+		
+		em.getTransaction().begin();
+		em.remove(toDelete);
+		em.flush();
+		em.getTransaction().commit();
+
+		log.info("Language removed by Admin");
+
+		Entitymanager.closeEntityManager();
+		return getAllLanguages();
 
 	}
 
--- src/main/java/com/fiyoteam/rest/SkillService.java
+++ src/main/java/com/fiyoteam/rest/SkillService.java
@@ -1,11 +1,14 @@
 package com.fiyoteam.rest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -13,7 +16,9 @@ import javax.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fiyoteam.model.DTO.UserSkillDTO.Skill;
+import com.fiyoteam.model.ProjectSkill;
+import com.fiyoteam.model.Skill;
+import com.fiyoteam.model.UserSkill;
 import com.fiyoteam.persistence.Entitymanager;
 import com.sun.jersey.spi.container.ResourceFilters;
 
@@ -37,6 +42,48 @@ public class SkillService {
 
 		Entitymanager.closeEntityManager();
 		return Response.ok(skillList).build();
+	}
+	
+	@GET
+	@Path("/{newSkill}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ResourceFilters(MyRequestFilter.class)
+	public Response addNewSkill(@PathParam("newSkill") String newSkill) {
+		EntityManager em = Entitymanager.getEntityManagerInstance();
+		
+		Skill skill = new  Skill(0, newSkill, new ArrayList<UserSkill>(), new ArrayList<ProjectSkill>());
+		
+		em.getTransaction().begin();
+		em.persist(skill);
+		em.flush();
+		em.getTransaction().commit();
+
+		log.info("New skill added by Admin");
+
+		Entitymanager.closeEntityManager();
+		return getAllLanguages();
+
+	}
+	
+	@DELETE
+	@Path("/{skillId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ResourceFilters(MyRequestFilter.class)
+	public Response deleteSkill(@PathParam("skillId") Integer skillId) {
+		EntityManager em = Entitymanager.getEntityManagerInstance();
+		
+		Skill toDelete = em.find(Skill.class, skillId);
+		
+		em.getTransaction().begin();
+		em.remove(toDelete);
+		em.flush();
+		em.getTransaction().commit();
+
+		log.info("Skill removed by Admin");
+
+		Entitymanager.closeEntityManager();
+		return getAllLanguages();
+
 	}
 
 }
